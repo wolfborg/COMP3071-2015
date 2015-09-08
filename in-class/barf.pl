11ad696ad979c4eb8feb93f662af5aec1efb0b93
Joey Lawrance <lawrancej@wit.edu>
2015-09-08 15:01:52 -0400

    Bam

---
+++ in-class/barf.pl
@@ -0,0 +1,24 @@
+f(a).
+f(b).
+g(a).
+g(b).
+h(b).
+k(X):- f(X), h(X), g(X).
+
+/* child(X,Y) means X is the child of Y */
+child(anna,bridget).
+child(bridget,caroline).
+child(caroline,donna).
+child(donna,emily).
+
+/* Recursion example */
+member(X,[Y|Z]):- X=Y; member(X,Z).
+
+/*
+    descend(X,Y) right now means X is the child or grandchild of Y.
+    try making it work for child, grandchild, great-grandchild, etc.
+    through recursion.
+ */
+descend(X,Y):- child(X,Y).
+descend(X,Y):- child(X,Z), child(Z,Y).
+
--- in-class/intro.pl
+++ in-class/intro.pl
@@ -1,4 +1,3 @@
-
 /*
 Introduction to Prolog predicates, atoms, variables, facts and rules.
 
@@ -11,7 +10,12 @@ Anything that starts with an uppercase letter is a variable.
 loves (defined below) is a predicate. The names are atoms, X is a variable.
 
 These are facts.
+
+loves has "arity" 2. (It has 2 arguments)
+loves/2
+
 */
+
 loves(joey,X).        /* joey loves everybody (X is a variable) */
 loves(X,raymond).     /* everybody loves raymond (X is a variable) */
 loves(john,bill).     /* john loves bill */
@@ -20,12 +24,16 @@ loves(bill,susan).    /* bill loves susan */
 loves(bill,ian).      /* bill loves ian */
 loves(susan,bill).    /* suan loves bill */
 
+loves(me).
+
+/* man/1 */
 man(joey). /* joey is a man */
 man(john). /* john is a man */
 man(bill). /* bill is a man */
 man(ian).  /* ian is a man */
 man(raymond). /* raymond is a man */
 
+/* woman/1 */
 woman(sally). /* sally is a woman */
 woman(susan).
 
@@ -47,4 +55,15 @@ true_love(X,Y):- loves(X,Y),loves(Y,X).
     () means group. (P ; Q), R. reads as: (P or Q) and R.
 */
 jealous(X,Y,Z):- (loves(X,Z),loves(Y,Z));(loves(Z,X),loves(Z,Y)).
+
+/*
+    member/2
+    [] is the empty list
+    [Head|Tail]
+    is X a member of the list [Y followed by Z]?
+    is X = Y? we're done
+    if not, then check to see if X is a member of the list Z.
+*/
+member(X,[Y|Z]):- X=Y; member(X,Z).
+
 
