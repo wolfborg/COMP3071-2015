7acc544fba5e59035d10269aee1fe275b6c0e2ae
Joey Lawrance <lawrancej@wit.edu>
2015-09-08 21:44:59 -0400

    Descend.

--- in-class/barf.pl
+++ in-class/barf.pl
@@ -18,7 +18,9 @@ member(X,[Y|Z]):- X=Y; member(X,Z).
     descend(X,Y) right now means X is the child or grandchild of Y.
     try making it work for child, grandchild, great-grandchild, etc.
     through recursion.
- */
 descend(X,Y):- child(X,Y).
 descend(X,Y):- child(X,Z), child(Z,Y).
+ */
 
+descend(X,Y):- child(X,Y). /* base case */
+descend(X,Z):- child(X,Y),descend(Y,Z). /* recursive case */
