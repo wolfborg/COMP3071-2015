c62fe88de1282adf619b0c23d033fa1c9b0a589e
Joey Lawrance <lawrancej@wit.edu>
2015-09-18 13:17:53 -0400

    Derp, fixed.

--- labs/Lab1.hs
+++ labs/Lab1.hs
@@ -4,27 +4,29 @@ import Data.List
 -- Fix the syntax error so that myLast [1 .. 6] returns 6
 -- Note: I won't accept myLast = last
 myLast :: [a] -> a
+myLast = undefined
 
 -- Return whether the list is a palindrome
 -- Fix the error so that isPalindrome "tacocat" is true
 isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome = undefined
 
 -- Hailstone sequence
 -- http://en.wikipedia.org/wiki/Hailstone_sequence
 -- Write a program that computes the hailstone sequence for a given number
 -- For example, hailstone 5 -> [5 16 8 4 2 1]
 hailstone :: Integer -> [Integer]
-
+hailstone = undefined
 
 -- Write a function to convert a number into a list of digits
 -- For example, toDigits 30 -> [3, 0]
 unpack :: Integer -> [Integer]
-
+unpack = undefined
 
 -- Write a function to convert a list of digits into a number
 -- For example, fromDigits [3, 4, 5] -> 345
 pack :: [Integer] -> Integer
-
+pack = undefined
 
 -- Kaprekar's routine: 
 -- http://en.wikipedia.org/wiki/6174_%28number%29
@@ -36,15 +38,17 @@ pack :: [Integer] -> Integer
 -- For example, sort [ 1, 2, 5, 4] -> [1, 2, 4, 5]
 -- reverse [1, 3, 4] -> [4, 3, 1]
 kaprekar :: Integer -> Integer
-
+kaprekar = undefined
 
 -- Kaprekar's list
 -- Write a function that computes Kaprekar's routine for a number repeatedly until it reaches its fixed point
 -- For example, kaprekarList 5432 -> [5432, 3087, 8352, 6174]
 kaprekarList :: Integer -> [Integer]
+kaprekarList = undefined
 
 -- Spelling alphabet
 -- Translate a string into a string spelled out using the NATO phonetic alphabet
 -- http://en.wikipedia.org/wiki/NATO_phonetic_alphabet
 -- For example, say "Fearing" -> "Foxtrot Echo Alpha Romeo India November Golf"
 say :: String -> String
+say = undefined
