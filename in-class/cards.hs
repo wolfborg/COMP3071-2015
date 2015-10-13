2dcba090d282a8dc9b672d651f69b7aef786ddac
Joey Lawrance <lawrancej@wit.edu>
2015-10-13 15:03:18 -0400

    Demonstrate models of computation and haskell modules

--- in-class/cards.hs
+++ in-class/cards.hs
@@ -1,4 +1,8 @@
--- Let's play war
+-- 
+import System.Random
+import Data.Map as Map
+
+-- Let's play cards
 data Suit = Spades | Hearts | Clubs | Diamonds deriving (Show, Eq, Enum)
 
 -- No matter what the suit happens to be
@@ -11,7 +15,34 @@ data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack 
 data Card = Card {
     rank :: Rank,
     suit :: Suit
-} deriving (Show, Eq, Ord)
+} deriving (Eq, Ord)
 
--- Card Ace Spades
+instance Show Card where
+    show (Card rank suit) = (show rank) ++ " of " ++ (show suit)
+
+-- Pack of cards
 pack = [Card r s | r <- [Two .. Ace], s <- [Spades .. Diamonds]]
+
+data Facing = Up | Down
+
+data Pile {
+    cards :: [Card],
+    direction :: Facing
+}
+
+-- The idea is that maybe we number the piles, or refer to them by name or an enum
+data Game a {
+    table :: Map a Pile
+}
+
+-- Source: http://en.literateprograms.org/Fisher-Yates_shuffle_%28Haskell%29
+shuffle :: [a] -> IO [a]
+shuffle l = shuffle' l []
+
+shuffle' [] acc = return acc
+shuffle' l acc =
+  do k <- randomRIO (0, length l - 1)
+     let (lead, x:xs) = splitAt k l
+     shuffle' (lead ++ xs) (x:acc)
+
+-- deal
