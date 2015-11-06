18b3165d939db963634140996bef4a3dad6d2adf
Joey Lawrance <lawrancej@wit.edu>
2015-11-06 13:47:26 -0500

    Added calculator stuff.
    *HINT* Study Printer.java

---
+++ language/src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator extends ExprBaseVisitor<Double> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override
+	public Double visitProg(ExprParser.ProgContext ctx) {
+		return visitChildren(ctx);
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override
+	public Double visitExpr(ExprParser.ExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+}
